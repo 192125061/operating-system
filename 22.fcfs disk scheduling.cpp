2e9e316fdf50fbfa088d640981d9f09793ade02b
AK.Sathish Kumar <112234185+192125061@users.noreply.github.com>
2022-09-23 06:08:38 +0530

    Update 22.fcfs disk scheduling.cpp

--- 22.fcfs disk scheduling.cpp
+++ 22.fcfs disk scheduling.cpp
@@ -1,43 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int size = 8;
-
-void FCFS(int arr[], int head)
-{
-	int seek_count = 0;
-	int distance, cur_track;
-
-	for (int i = 0; i < size; i++) {
-		cur_track = arr[i];
-
-	
-		distance = abs(cur_track - head);
-
-		seek_count += distance;
-
-		head = cur_track;
-	}
-
-	cout << "Total number of seek operations = "
-		<< seek_count << endl;
-
-	cout << "Seek Sequence is" << endl;
-
-	for (int i = 0; i < size; i++) {
-		cout << arr[i] << endl;
-	}
-}
-
-
+#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-
-
-	int arr[size] = { 176, 79, 34, 60, 92, 11, 41, 114 };
-	int head = 50;
-
-	FCFS(arr, head);
-
-	return 0;
+    int RQ[100],i,n,TotalHeadMoment=0,initial;
+    printf("Enter the number of Requests\n");
+    scanf("%d",&n);
+    printf("Enter the Requests sequence\n");
+    for(i=0;i<n;i++)
+     scanf("%d",&RQ[i]);
+    printf("Enter initial head position\n");
+    scanf("%d",&initial);
+    
+    // logic for FCFS disk scheduling
+    
+    for(i=0;i<n;i++)
+    {
+        TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+        initial=RQ[i];
+    }
+    
+    printf("Total head moment is %d",TotalHeadMoment);
+    return 0;
+    
 }
